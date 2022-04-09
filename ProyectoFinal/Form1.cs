3047eb8a127e0c711012fb2d58186155f99cd894
Euris Alexander Aquino <75188409+EurisAquino@users.noreply.github.com>
2022-04-09 13:00:48 -0400

    Add project files.

---
+++ ProyectoFinal/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoFinal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Pongan su matricula para ver que pudieron entrar
+            //2019-8874 Euris Aquino
+        }
+    }
+}
